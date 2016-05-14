eee4d3fc55efcfa74899cad6471752558db3fc9b
Renã Oliveira <rena.p.oliveira@gmail.com>
2016-05-14 10:23:12 -0300

    Changing variable declaration style

--- static-methods-and-variables/index.php
+++ static-methods-and-variables/index.php
@@ -3,36 +3,36 @@
 //A property declared as static cannot be accessed with an instantiated class object (though a static method can).
 class Test
 {
-    public static $static_var = "Static Variable";
+    public static $staticVar = "Static Variable";
 
     function __construct()
     {
         //The code below is creating a new property and not adding a value to the static variable
-        $this->static_var = "Instance property";
+        $this->staticVar = "Instance property";
     }
 
     public function normalMethod()
     {
-        return $this->static_var;
+        return $this->staticVar;
     }
 
     //Static methods are not related to the "$this" instance and cannot access it.
     public static function staticMethod()
     {
-        return self::$static_var;
+        return self::$staticVar;
     }
 }
 
 $mytest = new Test();
 print $mytest->normalMethod() . "\n";
-print $mytest->static_var . "\n";
+print $mytest->staticVar . "\n";
 
 //To access static methods and variables
 print Test::staticMethod() . "\n";
-print Test::$static_var . "\n";
+print Test::$staticVar . "\n";
 
 $classname = "Test";
-print $classname::$static_var . "\n"; // As of PHP 5.3.0
+print $classname::$staticVar . "\n"; // As of PHP 5.3.0
 print $classname::staticMethod() . "\n"; // As of PHP 5.3.0
 
 //The code below does not work.
